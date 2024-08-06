7900ed204a9f38f079c57635ec50a98e58b313b9
Pablo Castro <pablocastrodb@gmail.com>
2024-08-06 13:37:09 +0200

    initial architecture:  Domain - Data - Presentation

---
+++ Rick&MortyWiki/Rick&MortyWiki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rick&MortyWiki
+//
+//  Created by Pablo Castro on 6/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rick&MortyWiki/Rick&MortyWiki/Rick_MortyWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyWikiApp.swift
+//  Rick&MortyWiki
+//
+//  Created by Pablo Castro on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
